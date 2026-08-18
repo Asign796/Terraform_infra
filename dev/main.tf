7ff210e9e8d640370779711d164250654cdbbd07
an796 <anamika.singh510@gmail.com>
2026-08-19 00:55:13 +0530

    infra

--- dev/main.tf
+++ dev/main.tf
@@ -1,37 +1,37 @@
 module "create_rg" {
-  source   = "../../../module/azurerm_rg"
+  source   = "../module/azurerm_rg"
   child_rg = var.root_rg
 }
 
 module "create_stg" {
   depends_on      = [module.create_rg]
-  source          = "../../../module/azurerm_storage_account"
+  source          = "../module/azurerm_storage_account"
   storage_account = var.root_stg
 }
 
 module "create_network" {
   depends_on      = [module.create_rg]
-  source          = "../../../module/azurerm_networking"
+  source          = "../module/azurerm_networking"
   virtual_network = var.root_network
 
 }
 
 module "create_aks" {
   depends_on = [module.create_network]
-  source     = "../../../module/azurerm_kubernetese_cluster"
+  source     = "../module/azurerm_kubernetese_cluster"
   AKS        = var.root_AKS
 }
 
 
 module "create_acr" {
   depends_on = [module.create_rg]
-  source     = "../../../module/Azurerm_ACR"
+  source     = "../module/Azurerm_ACR"
   ACR        = var.root_ACR
 
 }
 
 module "create_appgw" {
-  source = "../../../module/azurerm_applicationgateway"
+  source = "../module/azurerm_applicationgateway"
 
   appgw = var.root_APPGW
 
@@ -41,7 +41,7 @@ module "create_appgw" {
 }
 
 module "create_appgw_public_ip" {
-  source     = "../../../module/Azurerm_Public_ip"
+  source     = "../module/Azurerm_Public_ip"
   appgw      = var.root_publicip
   depends_on = [var.root_network]
 }
